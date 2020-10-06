58d093049970b1304b9c2893139563c39864c213
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-10-06 19:23:33 -0400

    Adding files for meme

---
+++ meme_v_1/sketch.js
@@ -0,0 +1,14 @@
+/* meme_v_1
+by brendan doherty
+*/
+
+function setup(){
+    createCanvas(640,360);
+}
+
+function draw() {
+    background(220);
+
+    textSize(mouseY);
+    text('Brendan', width / 2, height / 2);
+}
