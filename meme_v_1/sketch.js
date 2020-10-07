32bd3512a3b88d6f9618c26f0eec5067f4ea68df
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-10-06 22:52:39 -0400

    Update text to new project

--- meme_v_1/sketch.js
+++ meme_v_1/sketch.js
@@ -9,6 +9,13 @@ function setup(){
 function draw() {
     background(220);
 
-    textSize(mouseY);
-    text('Brendan', width / 2, height / 2);
+    textSize(100);
+    
+    fill(144, 85, 167);
+    stroke(255)
+    textFont('monospace')
+    
+    textAlign(CENTER, CENTER);
+
+    text('Brendan', width / 3, height / 3);
 }
