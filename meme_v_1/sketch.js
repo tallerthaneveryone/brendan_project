516c0e21d9512b0f4e8b6ee543996bb56383d047
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-10-07 00:01:19 -0400

    updates to meme project

--- meme_v_1/sketch.js
+++ meme_v_1/sketch.js
@@ -2,20 +2,34 @@
 by brendan doherty
 */
 
+
 function setup(){
+    
     createCanvas(640,360);
+    
 }
 
 function draw() {
     background(220);
+    
+   
 
-    textSize(100);
+
+
+
+
+
+
+
+    textSize(30);
     
     fill(144, 85, 167);
     stroke(255)
     textFont('monospace')
     
-    textAlign(CENTER, CENTER);
+    textAlign(LEFT, CENTER);
 
-    text('Brendan', width / 3, height / 3);
+    text('Taller Than Everyone', width / 9, height / 9);
+
+    
 }
