62cde82f6d3847e30d4cae95b720ee74fdf033d6
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-11-17 17:37:21 -0500

    Adding the movable meme

--- meme_v_3/sketch.js
+++ meme_v_3/sketch.js
@@ -5,17 +5,33 @@ animated meme/logo
 
 var x = 0;
 var y = 100; 
-var s = 100;
+var s = 100;// size
 var xSpeed = 3;
 var ySpeed = 3;
 
+var c; // color
+
+
 function setup(){
     createCanvas(640, 360);
+    noStroke();
+
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+
+    c = color(r, g, b);
+
 }
 
 function draw() {
     background(220);
 
+    fill(c);
+    //ellipse(x, y, s)
+    textSize(s);
+    textAlign(Center, )
+    text("DVD", x, y);
     ellipse(x, y, s);
 
     x += xSpeed;
@@ -25,11 +41,21 @@ function draw() {
     if ( x > width ||x < 0 ) {
         // reverse speed
         xSpeed *=  -1;
+        var r = random(255);
+        var g = random(255);
+        var b = random(255);
+    
+        c = color(r, g, b);
     }
        //hits top or bottom side of the canvas//
     if ( y > height || y < 0 ) {
         // reverse speed
         ySpeed *=  -1;
+        var r = random(255);
+        var g = random(255);
+        var b = random(255);
+    
+        c = color(r, g, b);
     }
 
 }
