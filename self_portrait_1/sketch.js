18fa9828a3b6958800680998b7c62e97287fe911
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-11-22 13:15:49 -0500

    changes to the correct page

--- self_portrait_1/sketch.js
+++ self_portrait_1/sketch.js
@@ -1,19 +1,31 @@
-/* 
-sound sampler v 1
+/*
+self portrait version 1 
+by Brendan Doherty\
 */
 
-var vanjieSound;
-
-function preload() {
-  vanjieSound = loadsound('vanjie.mp3');
-}
-
 function setup() {
-  createCanvas(640, 360);
-  vanjieSound.play();
-}
-
-
-function draw() {
-  background(255, 192, 203);
-}
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+   noStroke();
+     background(5,75,5);
+    
+    //face shape
+    fill(55,mouseY,55);
+    ellipse(210,200,160,500);
+    
+    //nose
+    fill(120);
+    triangle(200,80,100,240,230,240);
+    
+    //mouth
+    fill(255,0,0);
+    ellipse(200, map(mouseY,0,height,250,300), 150, 100);
+    
+    //eyeballs
+    fill(200);
+    ellipse(155,75,20,20);
+    ellipse(250,75,20,20);
+  
+  }
--- soundkeyboard/sketch.js
+++ soundkeyboard/sketch.js
@@ -1,73 +1,21 @@
-/* 
-meme v3
-animated meme/logo
+/*
+sound sampler v 1
 */
 
-var x = 0;
-var y = 100; 
-var s = 25;// size
-var xSpeed = 3;
-var ySpeed = 3;
-var c; // color
+var vanjieSound;
 
-
-var planeImage;
-
-function preload() {
-    planeImage = loadImage('plane.png');
+function preload () {
+    vanjieSound = loadSound('vanjie.mp3')
 }
 
-
-function setup(){
-    createCanvas(640, 360);
-    noStroke();
-
-    var r = random(255);
-    var g = random(255);
-    var b = random(255);
-
-    c = color(r, g, b);
-
+function setup() {
+    createCanvas(64,360);
 }
 
 function draw() {
     background(220);
-
-    tint(c);
-    image(planeImage, x, y, s, s);
-
-    fill(c);
-    //ellipse(x, y, s)
-    textSize(s);
-    textAlign(CENTER, CENTER);
-    text('landing or departing', x, y, s);
-    
-
-    
-
-    x += xSpeed;
-    y += ySpeed;
-
-        //hits right or left side of the canvas//
-    if ( x > width ||x < 0 ) {
-        // reverse speed
-        xSpeed *=  -1;
-        var r = random(255);
-        var g = random(255);
-        var b = random(255);
-    
-        c = color(r, g, b);
-    }
-       //hits top or bottom side of the canvas//
-    if ( y > height || y < 0 ) {
-        // reverse speed
-        ySpeed *=  -1;
-        var r = random(255);
-        var g = random(255);
-        var b = random(255);
-    
-        c = color(r, g, b);
-    }
-
 }
-   
+
+function mousePressed() {
+    vangieSound.play();
+}
