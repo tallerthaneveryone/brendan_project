d35f7fea4c1ecc72800b274ab3e9ba83da82c01b
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-12-03 17:56:02 -0500

    update changes to the code for keyboard

--- keyboardsound/sketch.js
+++ keyboardsound/sketch.js
@@ -2,46 +2,22 @@
 sound keyboard v 1
 */
 
-
-var vanjieSound;
-
-
-function preload () {
-    vanjieSound = loadSound('vanjie.mp3');
-}
-
-function setup() {
-    createCanvas(640,360);
-    
+function setup () {
+    createCanvas(640, 360);
 }
 
 function draw() {
     background(220);
-
-    if (vanjieSound.isPlaying()) {
-        background(8, 40, 15);
-    }
-
-    var currentTime = vanjieSound.currentTime();
-    var duration = vanjieSound.duration();
-    var timeElapsed = map(currentTime, 0, duration, 0, width);
-
-    fill('0');
-    noStroke();
-    rect(0, height -200, timeElapsed, 200);
-
-    //change volume
-
-    var volume = map(mouseY, 0, height, 1, 0);
-    vanjieSound.setVolume(volume);
-
     
-}
-
-function mousePressed() {
-    if (vanjieSound.isPlaying()) {
-        vanjieSound.pause();
-    } else {
-    vanjieSound.play();
+    if (keyIsPressed) {
+        if (key == 'b');
     }
+
+    if (key == 'p');{
+        background('pink');
+    }
+
+    textSize(20);
+    text(keyIsPressed, 20, 20);
+
 }
