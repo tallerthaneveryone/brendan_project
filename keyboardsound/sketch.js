64082e5cb05870d7909361cbe230afd7287b6fda
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-12-06 01:41:41 -0500

    sound sampler keyboard

--- keyboardsound/sketch.js
+++ keyboardsound/sketch.js
@@ -2,9 +2,17 @@
 sound keyboard v 1
 */
 
+var cookingSound
+
+function preload(cookingSound) {
+    
+}
+
 function setup () {
     createCanvas(640, 360);
 }
+
+
 
 function draw() {
     background(220);
