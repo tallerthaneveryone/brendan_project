ae919a94d3e236786cefa87101f7734f126172fa
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-12-07 23:33:56 -0500

    updates to mmp210

---
+++ pattern1/sketch.js
@@ -0,0 +1,18 @@
+/* pattern version 1
+javascript loops
+*/
+
+function setup(){
+    createCanvas(640, 306);
+}
+
+function draw(){
+    background(220);
+}
+
+var i = 0;
+
+while (i<5){
+    i++;
+    console.log('i', i);
+}
