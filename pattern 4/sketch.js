0758bab9811b24398c56624b647711bbd42b23b2
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-12-15 09:03:09 -0500

    changes

--- pattern 4/sketch.js
+++ pattern 4/sketch.js
@@ -1,23 +1,33 @@
 /*
-nested loop
+generative pattern
 */
 
 function setup() {
 	createCanvas(600, 500);
+	pattern();
 }
 
-function draw() {
+function mousePressed()	{
+	pattern();
+}
+
+function pattern() {
 	background(200);
 
-	let w = 50;
+	let w = random(5, 100);
 	let h = 60;
 
-	for (let x = 10; x <= width; x += w) { // columns
-		for (let y = 20; y <= height; y += h) { // rows
-			rect(x, y, w, h);
-			circle(x, y, w, h);
-			triangle(x, y, w, h+=20);
+	for (let x = 10; x <= width + w; x += w) { // columns
+		for (let y = 20; y <= height + h; y += h) { // rows
+			
+			let r = random(255);
+			let g = random(255);
+			let b = random(255);
 
+			fill(r, g, b);
+			
+			circle(x, y, w, h);
+			
 		}	
 	}
 }
