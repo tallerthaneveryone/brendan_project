e61c130799131e1b7c5d5a7b0a13949703254da6
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-12-14 19:50:14 -0500

    Pattern v1

--- pattern1/sketch.js
+++ pattern1/sketch.js
@@ -1,18 +1,45 @@
-/* pattern version 1
-javascript loops
-*/
+/* pattern version 1 javascript loops*/
 
-function setup(){
-    createCanvas(640, 306);
+function setup() {
+	createCanvas(640, 360);
+	rectMode(CENTER);
 }
 
-function draw(){
-    background(220);
-}
+function draw() {
+	background(120);
 
-var i = 0;
+	// landscape
 
-while (i<5){
-    i++;
-    console.log('i', i);
+	// mountains
+	for (let x = -20; x < width; x += 80) {
+		
+		let r = map(x, 10, width, 100, 255);
+		fill(r, 185);
+		
+		stroke('white');
+		strokeWeight(6);
+		triangle(x, height + 18, x + 30, 60, x + 200, width);
+	}
+
+	// trees trunks
+	for (let x = 20; x < height; x += 40) {
+		noStroke();
+		fill('white');
+		rect(x, height - 20, 05, 200);
+    //leafs
+		let g = map(x, 90, width, 175, 60);
+		fill(10, g, 10);
+        circle(x, height - 120, 50, 90);
+	}
+
+	// clouds 
+	let y = 100;
+	for (let x = 20; x < width; x += 120) {
+		fill('white');
+
+        circle(x, y, 50);
+        quad(x - 20, y + 10, 50, 60);
+
+		y += 0;
+	}
 }
